2aba06ba783651863ca65c6bf999f64ced896a0e
Gusarov Vadim <gusarovvadim1996@gmail.com>
2018-05-15 19:22:59 +0500

    add cors

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 
 module.exports.getApp = async function () {
     const express = require('express');
+    const cors = require('cors');
     const bodyParser = require('body-parser');
     const app = express();
     const { getMongo } = require('./mongo');
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cors());
     let { client, maxSite } = await getMongo();
     require('./routes.js')(app, client.db('places').collection('places'), maxSite);
 
